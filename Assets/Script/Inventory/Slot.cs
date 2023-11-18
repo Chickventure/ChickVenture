85f38ff2629cbf2f222d6be751701d188f862a13
wfqfikri <103481159+wfqfikri@users.noreply.github.com>
2023-11-18 18:23:22 +0700

    inventory
    nmbah inventory

---
+++ Assets/Script/Inventory/Slot.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    private InventoyController inventory;
+    public int i;
+    public TextMeshProUGUI amountText;
+    public int amount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = GetComponent<InventoyController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        amountText.text = amount.ToString();
+
+        if (amount > 1)
+        {
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().enabled = true;
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<TextMeshProUGUI>().enabled = false;
+        }
+
+        if(transform.childCount == 10)
+        {
+            inventory.isFull[i] = false;
+        }
+    }
+
+    public void DropItem() 
+    { 
+        if (amount > 1)
+        {
+            amount -= 1;
+            transform.GetComponentInChildren<Spawn>().SpawnDropItem();
+        }
+        else
+        {
+            amount -= 1;
+            GameObject.Destroy( transform.GetComponentInChildren<Spawn>().gameObject);
+            transform.GetComponentInChildren<Spawn>().SpawnDropItem();
+        }
+
+    }
+}
---
+++ Assets/Script/Inventory/Spawn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject ItemPerfabs;
+    public Transform player;
+    public string itemName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+   public void SpawnDropItem()
+    {
+        Vector3 playerposition = new Vector3(player.position.x, player.position.y, player.position.z + 4);
+        Instantiate(ItemPerfabs, playerposition, Quaternion.identity);
+    }
+}
