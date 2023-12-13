dc4f5d45c806eacd8c8c7dcb90297cf0296c55fc
wfqfikri <103481159+wfqfikri@users.noreply.github.com>
2023-12-13 13:28:38 +0700

    a
    a

--- Assets/Script/Enemy/Enemy_sideaway.cs
+++ Assets/Script/Enemy/Enemy_sideaway.cs
@@ -1,11 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 public class Enemy_sideaway : MonoBehaviour
 {
+    [SerializeField] private float moveDistance;
+    [SerializeField] private float speed;
     [SerializeField] private float damage;
+    private bool movingUp;
+    private float upEdge;
+    private float downEdge;
 
+
+    private void Awake()
+    {
+        upEdge = transform.position.y - moveDistance;
+        downEdge = transform.position.y + moveDistance;
+    }
+
+    public void Update()
+    {
+        if (movingUp)
+        {
+            if(transform.position.y > upEdge)
+            {
+                transform.position = new Vector3(transform.position.x , transform.position.y - speed * Time.deltaTime, transform.position.z);
+                
+            }
+            else
+            {
+                movingUp = false;
+            }
+        }
+        else
+        {
+            if (transform.position.y < downEdge)
+            {
+
+                transform.position = new Vector3(transform.position.x , transform.position.y + speed * Time.deltaTime, transform.position.z);
+                
+            }
+            else
+            {
+                movingUp = true;
+            }
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player") 
@@ -13,4 +54,5 @@ public class Enemy_sideaway : MonoBehaviour
             collision.GetComponent<Health>().TakeDamage(damage);
         }
     }
+
 }
