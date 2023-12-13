adb34855bf5e3c5bf1c869c223104d9a2b660216
wfqfikri <103481159+wfqfikri@users.noreply.github.com>
2023-12-13 09:26:43 +0700

    a
    a

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -12,16 +12,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private int MoveSpeed;
 
-    public int nilaiKoin;
-    public Text textkoin;
-    public static PlayerController instance;
+    //COIN DAN ITEM
+    public int nilaiKoin, nilaiPeanuts, nilaiPerangkap, nilaiJagung;
+    public Text textkoin, TextPeanuts, TextPerangkap, TextJagung;
+    public static PlayerController instance, instance1, instance2, instance3;
+    
     private void Awake()
     {
         instance = this; 
+        instance1 = this;
+        instance2 = this;
+        instance3 = this;
     }
-
-
-
 
 
     // Start is called before the first frame update
@@ -32,16 +34,29 @@ public class PlayerController : MonoBehaviour
         //Memanggil Animasi Gerak
         myAnim = GetComponent<Animator>();
 
+        //coin
         nilaiKoin = PlayerPrefs.GetInt("coin");
-        textkoin.text = "= " + nilaiKoin.ToString();
+        textkoin.text = " " + nilaiKoin.ToString();
+
         Debug.Log(nilaiKoin);
 
-        if(SceneManager.GetActiveScene().name == "Level 1")
+        //item Peanuts
+        nilaiPeanuts = PlayerPrefs.GetInt("Peanuts");
+        TextPeanuts.text = "" + nilaiPeanuts.ToString();
+
+        //ITEM PERANGKAP
+        nilaiPerangkap = PlayerPrefs.GetInt("Perangkap");
+        TextPerangkap.text = "" + nilaiPerangkap.ToString();
+
+        //ITEM JAGUNG
+        nilaiJagung = PlayerPrefs.GetInt("Jagung");
+        TextJagung.text = "" + nilaiJagung.ToString();
+
+        //save scene
+        if (SceneManager.GetActiveScene().name == "Main Menu")
         {
             Resetsave();
         }
-
-
     }
 
     public void Resetsave()
